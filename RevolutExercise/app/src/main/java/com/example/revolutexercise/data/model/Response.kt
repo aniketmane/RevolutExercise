e05ac3744da1d7629a16a5e1193ec59b40020e42
Aniket <aniketnmane@gmail.com>
2020-06-10 21:37:20 +0100

    Initial project setup & files.

---
+++ RevolutExercise/app/src/main/java/com/example/revolutexercise/data/model/Response.kt
@@ -0,0 +1,6 @@
+package com.example.revolutexercise.data.model
+
+data class Response(
+    val baseCurrency: String,
+    val rates: Rates
+)
---
+++ RevolutExercise/app/src/main/java/com/example/revolutexercise/ui/view/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.revolutexercise.ui.view
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
